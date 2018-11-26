31f80b47f67f838913ba122e19d8b941314c7919
Nathan Cairns <nathancairns777@gmail.com>
2018-11-26 19:49:04 +1300

    Add game

--- examples/example.py
+++ examples/example.py
@@ -1,2 +1,58 @@
 #!/usr/bin/env python3
 
+
+# IMPORTS #
+
+
+from .context import game
+
+
+# CONSTANTS #
+
+
+YES = {'yes','y', ''}
+NO = {'no','n'}
+
+
+# FUNCTIONS #
+
+
+def main():
+    print("Monty Hall Simulator")
+
+    # Create a game with as many doors as required.
+    num_doors = int(input("How many doors would you like to test with?: "))
+    mhallgame = game.Game(num_doors)
+
+    first_door_idx = int(input("Select the number of the door you wish to"
+            + "open (1 to {}): ".format(num_doors)))
+
+    # Select the door and monty narrows down
+    first_door_idx = first_door_idx - 1
+    mhallgame.select_first_door(first_door_idx)
+    second_door = mhallgame.second_door_index()
+
+    # Check if user wants to change door
+    print("You selected door {}".format(first_door_idx + 1))
+    print("Monty reveals all other doors except for door {} contain goats".format(second_door + 1))
+    change_door = input("Do you wish to change from the door you have selected? (y/n): ")
+
+    correct = False
+    if change_door in YES:
+        correct = mhallgame.select_final_door(2)
+    elif change_door in NO:
+        correct = mhallgame.select_final_door(1)
+    else:
+        print("Error: Incorrect value inputted. Value must be y or n")
+
+    if correct:
+        print("Congrats you won a car!")
+    else:
+        print("Sorry you are going home with a goat")
+
+
+# MAIN #
+
+
+if __name__ == "__main__":
+    main()
--- mhall/game.py
+++ mhall/game.py
@@ -1,14 +1,13 @@
+#!/usr/bin/env python3
+
 # This class represents a game instance
-# Author: Nathan Cairns
+# Author: Nathan Cairns
 
 
 ## IMPORTS ##
 
 
-import .door
-
-
-## CONSTANTS ##
+import random
 
 
 ## CLASS ##
@@ -18,24 +17,81 @@ class Game:
 
 
     def __init__(self, num_doors):
+        """INIT"""
         self.num_doors = num_doors
+        self.door_one = None
+        self.door_two = None
+        self.doors = []
         self._generate_doors()
 
 
     def _generate_doors(self):
         """Generates the array of doors for the game"""
-        for num_doors:
-            # Todo populate array of doors and add to self
-            assert True
+        for _ in range(self.num_doors):
+            self.doors.append(False)
+
+        # Randomly select the door with the car
+        self.car_door = random.randint(0, self.num_doors - 1) 
+        self.doors[self.car_door] = True
 
 
-    def _open_all_other_doors(self, door_num):
+    def _open_all_other_doors(self):
         """Opens all other doors"""
+        if self.door_one == self.car_door:
+            second_door_range = list(range(0, self.door_one))
+            second_door_range.extend(range(self.door_one, self.num_doors - 1))
+            self.door_two = random.choice(second_door_range)
+        else:
+            self.door_two = self.car_door
 
 
-    def open_door(self, door_num):
-        """Opens a door"""
-        assert True
+    def _check_door(self, door_num):
+        """Checks to see if door num contains the car
+        Parameters:
+            door_num: the index of the door to check
+        Returns:
+        """
+        if door_num == self.car_door:
+            return True
+        return False
 
 
+    def second_door_index(self):
+        """Shows the other door narrowed down by monty
+        Returns:
+            The index of the door selected by monty
+        """
+        if self.door_one is None:
+            raise RuntimeError("Error: The select_first_door() function must"
+                    + " be called first")
+        return self.door_two
+
+
+    def select_first_door(self, door_num):
+        """selects a door
+        Parameters:
+            door_num -- the index of the door to select
+        """
+        print(self.car_door + 1)
+        if door_num > self.num_doors or door_num < 0:
+            raise ValueError("Error: Input must be between 0 and {}".format(num_doors))
+
+        self.door_one = door_num
+        self._open_all_other_doors()
+
+
+    def select_final_door(self, door_num):
+        """Selects the final door to open
+        Parameters:
+            door_num -- the door to open. Either 1 or 2
+        Returns: 
+            true if user selects right, false otherwise
+        """
+        print(self.car_door + 1)
+        if door_num == 1:
+            return self._check_door(self.door_one)
+        elif door_num == 2:
+            return self._check_door(self.door_two)
+        else:
+            raise ValueError("Error: Input must be either 1 or 2")
 
